35ca0f54477708bfe4b29891b03c805f6c9f881f
[choisojung] <[sojc31@gmail.com]>
2020-06-10 23:48:25 -0700

    finished updating static views and moving mock data to controllers

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,9 @@
 const about = (req, res)=>{
-    res.render('generic-text', { title: 'About'});
+    res.render('generic-text', { 
+        title: 'About',
+        content: 'WSHFC\'s encourages affordable housing development in the state of Washington. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. Nunc sed lorem ac nisi dignissim accumsan.'
+        
+    });
 };
 
 module.exports = {
--- app_server/controllers/projects.js
+++ app_server/controllers/projects.js
@@ -1,13 +1,52 @@
 const projectlist = (req, res)=>{
-    res.render('projects-list', { title: 'Your projects'});
+    res.render('projects-list', { 
+        title: 'Your projects',
+        pageHeader: {
+            title: 'WSHFC-Application Portal',
+            strapline: 'Register to start your application'
+        },
+        sidebar: 'Questions or Comments? Contact us at mhcf@wshfc.org',
+        projects: [{
+            name: 'Copper Landing',
+            address: '208 15th Ave'
+        },{
+            name: 'Reserve at Lynnwood',
+            address: '1000 104th Street'
+        }, {
+            name: 'The Confluence',
+            address: '5618 54th Street'
+        }]
+    });
 };
 
 const projectInfo = (req, res)=>{
-    res.render('project-info', { title: 'Project Info'});
+    res.render('project-info', { 
+        title: 'Copper Landing',
+        pageHeader: {title: 'Copper Landing'},
+        sidebar: {
+            context: 'Context'
+        },
+        project: {
+            name: 'Copper Landing',
+            address: '208 15th Ave',
+            sites: [{
+                siteName: 'Copper',
+                siteAddress: '208 15th Ave, Seattle WA'
+            },{
+                siteName: 'Landing',
+                siteAddress: '1093 23rd Ave, Seattle, WA'
+            }]
+        }
+    });
 };
 
 const addSite = (req, res)=>{
-    res.render('project-site-form', { title: 'Add site'});
+    res.render('project-site-form', { 
+        title: 'Add site',
+        pageHeader: {
+            title: 'Add site to Copper Landing'
+        }
+    });
 };
 
 module.exports = {

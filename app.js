ba61aaeac261f18006b37f4b9c1be5238d69599a
[choisojung] <[sojc31@gmail.com]>
2020-06-12 07:00:16 -0700

    updated w cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'app_public', 'build')));
 
 
 app.use('/api', (req, res, next)=>{
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
